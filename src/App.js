17a2663f23d31e6365677cdb3ccf8fd2822cbe48
Alex <casha.mironyuk.2001@gmail.com>
2023-03-19 17:32:52 +0200

    CRA and install eslint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import './App.scss';
-import { Route, Routes, } from 'react-router-dom';
-import Articles from './pages/Articles';
-import Header from './components/Header';
-import Article from './pages/Article';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Routes>
-        <Route index element={<Articles />} />
-        <Route path='/article/:id' element={<Article />} />
-      </Routes>
-    </div>
+    <div>App</div>
   );
 }
 
